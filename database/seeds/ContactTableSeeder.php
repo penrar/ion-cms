513c3be0bf2b8427ea2d881ff10fdd6be1a2b13c
vylest <ejpenner@gmail.com>
2018-05-01 22:46:24 +0000

    fixed customer user order seed

--- database/seeds/ContactTableSeeder.php
+++ database/seeds/ContactTableSeeder.php
@@ -11,6 +11,17 @@ class ContactTableSeeder extends Seeder
      */
     public function run()
     {
+        \App\Contact::create([
+            'user_id' => 3,
+            'first_name' => 'Customer',
+            'last_name' => 'User',
+            'phone_number' => '402-555-2322',
+            'address1' => '333 Westhaven Trl',
+            'city' => 'Omaha',
+            'state' => 'NE',
+            'zip_code' => '68105'
+        ]);
+
         factory(App\Contact::class, 80)->create();
     }
 }
